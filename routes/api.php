c6350adfe46af433af1ed6e04d64a2892fdffd11
Ali-101 <ali.mannoun.1@gmail.com>
2020-04-07 10:38:50 +0300

    Remove unused routes

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-//create and edit methods to provide a form .
-/*
-if the user doesn't exist then we use this route to create new empolyee or admin or client
-throughout the user_type identifier
- */
-
 /**
  * User Endpoints
  */
@@ -31,29 +24,9 @@ Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]
 Route::post('users/check-user-credentials', 'User\UserController@checkUserCredentials');
 Route::post('users/login', 'User\UserController@getLoginCredentials');
 /**
- * Client Endpoints
- */
-
-/**
- * Employee Endpoints
- */
-
-/**
- * Account Endpoints
- */
-Route::resource('accounts', 'Account\AccountController', ['except' => ['create', 'edit']]);
-/**
- * Admin Endpoints
- */
-/*
-
-/**
- * Company Endpoints
- */
-
-/**
  * Car Endpoints
  */
 Route::resource('cars', 'Car\CarController', ['only' => ['index', 'show']]);
 Route::resource('cars.specifications', 'Car\CarSpecificationsController', ['only' => ['index', 'show']]);
+//For testing...
 Route::resource('cars.images', 'Car\CarImageController', ['only' => ['store']]);
